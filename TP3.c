a7214096dcf92be5c8842b71e74e245412ad847e
Fanfnix <valentin@familledenis.net>
2024-10-14 12:35:04 +0200

    Update and rename TP3 to TP3.c

---
+++ TP3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
+char liste_personnage[4]
+
+for (int i = 0; i < 3; i++)
+{
+  printf("Choisissez personnage %d", i);
+  scanf(" %c", &liste_personnage[i])
+}
