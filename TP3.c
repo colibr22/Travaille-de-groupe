0e6afb06b0e7cef1a3d150376e1ac86c04fe39b6
Fanfnix <valentin@familledenis.net>
2024-10-14 12:37:50 +0200

    Update TP3.c

--- TP3.c
+++ TP3.c
@@ -3,10 +3,14 @@
 #include <string.h>
 #include <stdlib.h>
 
-char liste_personnage[4]
+char liste_personnage[4][100];
 
 for (int i = 0; i < 3; i++)
 {
   printf("Choisissez personnage %d", i);
-  scanf(" %c", &liste_personnage[i])
+  scanf(" %c", &liste_personnage[i]);
 }
+
+int alea = rand() % 3;
+
+char personnage = liste_personnage[alea];
